020e27aad5255fa1d08e239a28013a77c51e6878
sumansauravmay <sumansauravmay12345@gmail.com>
2024-10-22 18:10:37 +0530

    output based question

---
+++ Output/index.js
@@ -0,0 +1,30 @@
+// 1
+// const a = [1, 2, 3]
+// const b = a.map(ele=>ele>2)
+// console.log(b);
+// output:[ false, false, true ]
+
+// 2.
+// const a = [1, 2, 3]
+// const b = a.filter(ele=>ele*2)
+// console.log(b);
+// output: [ 1, 2, 3 ]
+
+// 3.    
+// const a = [1, 2, 3]
+// const b = a.forEach(ele=>ele>2)
+// console.log(b);
+//output: undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
