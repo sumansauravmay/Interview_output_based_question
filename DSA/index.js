c4dce57a76d034f1e515fbad5ce434a99bc6bfde
sumansauravmay <sumansauravmay12345@gmail.com>
2024-10-22 18:17:31 +0530

    reverse

--- DSA/index.js
+++ DSA/index.js
@@ -35,13 +35,51 @@
 
 // 2. At place of space put 00.
 
-let str="I am Suman";
-let bag="";
-for(let i=0;i<str.length;i++){
-if(str[i]==" "){
-    bag=bag+"00";
-}else{
-    bag+=str[i];
+// let str="I am Suman";
+// let bag="";
+// for(let i=0;i<str.length;i++){
+// if(str[i]==" "){
+//     bag=bag+"00";
+// }else{
+//     bag+=str[i];
+// }
+// }
+// console.log(bag);
+
+
+// 3. Reverse the array from k;
+
+let nums=[1,2,3,4,5,7,6,8];
+let k=3;
+let arr1=[];
+let arr2=[];
+
+for(let i=0;i<=k;i++){
+    arr1.push(nums[i])
 }
+for(let j=k+1;j<nums.length;j++){
+    arr2.push(nums[j])
 }
-console.log(bag);
+let arr=[...arr2,...arr1];
+console.log(arr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
