3f8782c01dda3c4ca08b09c3d6223de0ed628588
sumansauravmay <sumansauravmay12345@gmail.com>
2024-10-22 19:56:39 +0530

    reverse string

--- DSA/index.js
+++ DSA/index.js
@@ -66,21 +66,37 @@
 
 // 4. Reverse the numbers given in the the bag. types is number initially
 
-let nums=123456;
-let str=nums.toString();
-let bag="";
-for(let i=str.length-1;i>=0;i--){
-bag+=str[i];
+// let nums=123456;
+// let str=nums.toString();
+// let bag="";
+// for(let i=str.length-1;i>=0;i--){
+// bag+=str[i];
+// }
+// console.log(bag);
+
+
+// 5. Reverse the string whose position is event.
+
+let str = "I am suman saurav";
+let arr = str.trim().split(" ");
+
+let bag = "";
+for (let i = 0; i < arr.length; i++) {
+  if (i % 2 === 1) {
+    for (let j = arr[i].length - 1; j >= 0; j--) {
+      bag += arr[i][j];
+    }
+  } else {
+    bag += arr[i];
+  }
+  
+
+  if (i < arr.length - 1) {
+    bag += " ";
+  }
 }
+
 console.log(bag);
-
-
-
-
-
-
-
-
 
 
 
