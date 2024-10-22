ee03b8b09700bc20f2f7eb828900684c014c65e3
sumansauravmay <sumansauravmay12345@gmail.com>
2024-10-22 20:17:29 +0530

    const reassign

--- Output/index.js
+++ Output/index.js
@@ -17,14 +17,35 @@
 //output: undefined
 
 
+// 4.
+
+// const a = [1, 2, 3];
+// a[7] = 6;
+// console.log(a);
+
+// output: [ 1, 2, 3, <4 empty items>, 6 ]
+
+// 5. 
+
+// const obj={
+//     a:1,
+//     b:2
+// }
+
+// obj["k"]=5;
+// console.log(obj)
+
+// output: { a: 1, b: 2, k: 5 }
 
 
+6. 
 
+const obj={
+    a:1,
+    b:2
+}
 
+obj[k]=5;
+console.log(obj)
 
-
-
-
-
-
-
+// output: error(k is not defined)
