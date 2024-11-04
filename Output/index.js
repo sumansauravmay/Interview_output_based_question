2711609e8490ea983024cb2b0437550e7892e9da
sumansauravmay <sumansauravmay12345@gmail.com>
2024-11-04 18:08:47 +0530

    type based questions

--- Output/index.js
+++ Output/index.js
@@ -10,12 +10,11 @@
 // console.log(b);
 // output: [ 1, 2, 3 ]
 
-// 3.    
+// 3.
 // const a = [1, 2, 3]
 // const b = a.forEach(ele=>ele>2)
 // console.log(b);
 //output: undefined
-
 
 // 4.
 
@@ -25,7 +24,7 @@
 
 // output: [ 1, 2, 3, <4 empty items>, 6 ]
 
-// 5. 
+// 5.
 
 // const obj={
 //     a:1,
@@ -37,15 +36,46 @@
 
 // output: { a: 1, b: 2, k: 5 }
 
+// 6.
 
-6. 
+// const obj={
+//     a:1,
+//     b:2
+// }
 
-const obj={
-    a:1,
-    b:2
-}
-
-obj[k]=5;
-console.log(obj)
+// obj[k]=5;
+// console.log(obj)
 
 // output: error(k is not defined)
+
+// 7.
+
+// (()=>{
+//     let a=b=10;
+// })();
+
+// console.log(b);//10
+// console.log(a);//a is not defined
+
+// 8.
+
+// function name2(){
+//     let a=10;
+// }
+
+// name2();
+// console.log(a)//a is not defined
+
+// 9.
+// console.log(typeof undefined); //undefined
+// console.log(typeof NaN); //number
+// console.log(typeof null); //object
+// console.log([] == []); //false
+// console.log([] === []); //false
+// console.log({} == {}); //false
+// console.log({} === {}); //false
+
+// console.log(undefined == null); //true
+// console.log(undefined === null); //false
+// console.log(NaN == NaN);//false
+// console.log(NaN === NaN);//false
